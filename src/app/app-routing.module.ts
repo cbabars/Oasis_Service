e8f14209f0bf1477e97eb04bec56706ffd4646c4
Sagar Babar <sagar.babar@globeflexresearch.com>
2020-01-20 13:02:23 +0530

    added home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
