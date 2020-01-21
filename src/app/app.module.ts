289587ee2cb0fa3a7f8472c028a6bdaeab8c84b3
Sagar Babar <sagar.babar@globeflexresearch.com>
2020-01-21 17:53:38 +0530

    added scroll to top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { GoTopButtonModule } from 'ng2-go-top-button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GoTopButtonModule,
+    BrowserAnimationsModule
+  ],
   declarations: [
     AppComponent,
     HomePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
